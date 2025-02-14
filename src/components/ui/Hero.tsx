845733d574bf0a73e10a71266645329341663c34
Atheeb <177762114+heetabofficial@users.noreply.github.com>
2025-02-15 01:17:14 +0530

    changed(hero): new hero copy one liner

--- src/components/ui/Hero.tsx
+++ src/components/ui/Hero.tsx
@@ -36,7 +36,7 @@ export const Hero = () => {
           </div>
           <div className="flex gap-4 flex-col">
             <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
-              <span className="text-spektr-cyan-50">This is something</span>
+              <span className="text-spektr-cyan-50">We are building something</span>
               <span className="relative flex w-full justify-center overflow-hidden text-center md:pb-4 md:pt-1">
                 &nbsp;
                 {titles.map((title, index) => (
