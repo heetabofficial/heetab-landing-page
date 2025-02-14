0bcc9763e8aca74c077dd15e891e92aca96b4d3b
Atheeb <177762114+heetabofficial@users.noreply.github.com>
2025-02-15 01:09:52 +0530

    added(feature): some pages are added to the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { FAQ } from "@/components/ui/FAQ";
 import { Footer } from "@/components/ui/Footer";
 import { Header } from "@/components/ui/Header";
 import { Hero } from "@/components/ui/Hero";
+import { Stats } from "@/components/ui/stats";
+import { Case } from "@/components/ui/testimonila";
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <div>
         <Header />
         <Hero />
+        <Case />
+        <Stats />
         <FAQ />
         <Footer />
       </div>
